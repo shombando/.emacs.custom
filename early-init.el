40deb0619d74bb6376bbdd1eb8fc8fba71683e43
Shom Bandopadhaya <shom@bandopadhaya.com>
2021-11-16 18:05:08 -0600

    Performance improvements from daviwil and hlissner

---
+++ early-init.el
@@ -0,0 +1,19 @@
+;; -*- lexical-binding: t; -*-
+
+;; The default is 800 kilobytes.  Measured in bytes.
+(setq gc-cons-threshold (* 50 1000 1000))
+
+;; Profile emacs startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "*** Emacs loaded in %s seconds with %d garbage collections."
+                     (emacs-init-time "%.2f")
+                     gcs-done)))
+
+;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
+;; packages are compiled ahead-of-time when they are installed and site files
+;; are compiled when gccemacs is installed.
+;; REVIEW Remove after a month
+(setq comp-deferred-compilation nil
+      native-comp-deferred-compilation nil)
+
