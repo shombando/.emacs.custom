702349a3bfa84deb46bebc751be5fe07e6e92c0b
Shom Bandopadhaya <shom@bandopadhaya.com>
2021-11-22 19:56:58 -0600

    Switch windows quicker

--- init.el
+++ init.el
@@ -279,6 +279,7 @@ targets."
 (setq org-log-done 'time)
 
 (global-set-key (kbd "C-s") 'save-buffer)
+(global-set-key (kbd "M-a") 'other-window)
 (global-set-key (kbd "C-S-p") 'execute-extended-command)
 (global-set-key (kbd "C-<tab>") #'consult-buffer)
 (global-set-key (kbd "C-f") #'consult-ripgrep)
