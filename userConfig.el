99d990366356be8d8b4801a6a9989da9c333fcb4
Shom Bandopadhaya <shom@bandopadhaya.com>
2021-12-07 21:17:08 -0600

    Email config for mu4e using mbsync (conf below)
    ~/.mbysncrc
    IMAPAccount proton
    Host 127.0.0.1
    Port 1143
    User user@example.com
    PassCmd "cat ~/.protonBridgePass" #insecure but local only
    SSLType NONE
    CertificateFile /etc/ssl/certs/ca-certificates.crt
    
    IMAPStore proton-remote
    Account proton
    
    MaildirStore proton-local
    Subfolders Verbatim
    Path ~/mail/proton/
    Inbox ~/mail/proton/inbox/
    
    Channel proton
    Far :proton-remote:
    Near :proton-local:
    Patterns *
    Create Both
    SyncState *

--- userConfig.el
+++ userConfig.el
@@ -44,3 +44,32 @@ See `org-capture-templates' for more information."
 
 (use-package yaml-mode
   :straight t)
+
+(use-package mu4e
+  :straight nil
+  :defer 20 ; Wait until 20 seconds after startup
+  :config
+
+  (setq mu4e-change-filenames-when-moving t ; avoid sync conflicts
+	mu4e-update-interval (* 10 60) ; check mail 10 minutes
+	mu4e-get-mail-command "mbsync -a"
+	mu4e-maildir "~/mail/proton")
+
+  (setq mu4e-drafts-folder "/proton/Drafts"
+	mu4e-sent-folder   "/proton/Sent"
+	mu4e-refile-folder "/proton/All Mail"
+	mu4e-trash-folder  "/proton/Trash")
+
+  (setq mu4e-maildir-shortcuts
+      '(("/proton/inbox"     . ?i)
+        ("/proton/Sent"      . ?s)
+        ("/proton/Trash"     . ?t)
+        ("/proton/Drafts"    . ?d)
+        ("/proton/All Mail"  . ?a)))
+
+  (setq message-send-mail-function 'smtpmail-send-it
+	auth-sources '("~/.authinfo") ;need to use gpg version but only local smtp stored for now
+	smtpmail-smtp-server "127.0.0.1"
+	smtpmail-smtp-service 1025
+	smtpmail-stream-type  'ssl))
+
