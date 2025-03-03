2b08d4b6dfedd99fd97a1882d63754c4d2501d8c
Shom Bandopadhaya <shom@bandopadhaya.com>
2025-03-03 11:34:58 -0600

    Switching to Codeberg as primary

--- setRemotes.sh
+++ setRemotes.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/sh
+set -eu
+git remote set-url --push origin --add git@codeberg.org:shom/.emacs.custom.git 
 git remote set-url --push origin --add git@git.sr.ht:~shom/.emacs.custom
 git remote set-url --push origin --add git@github.com:shombando/.emacs.custom.git
-git remote set-url --push origin --add git@codeberg.org:shom/.emacs.custom.git
