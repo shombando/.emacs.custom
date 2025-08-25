7465bfa2f0aa26933147262d3e4e21299f1ebade
Shom Bandopadhaya <shom@bandopadhaya.com>
2025-08-25 00:15:55 -0500

    Edit file directly over SSH, no save hook required

--- modules/org-social.el
+++ modules/org-social.el
@@ -2,18 +2,10 @@
 	 :straight( :package "org-social"
 							:type git :host github :repo "tanrax/org-social.el")
 	:config
-	(setq org-social-file "~/dev/shom.dev/static/social.org")
+	(setq org-social-file "/ssh:conmanBananerd:/home/conman/prod/caddy/site/shom.dev/social.org")
 	(add-to-list 'evil-emacs-state-modes 'org-social-timeline-mode)
 	(evil-leader/set-key
 		"s" '("social"     . (keymap))
 		"st" '("timeline"  . org-social-timeline)
 		"sn" '("new"       . org-social-new-post)
 		"so" '("open"      . org-social-open-file))
-	:init
-	(add-hook 'org-social-after-save-file-hook
-						(lambda ()
-							(call-process-shell-command
-							 (format "scp %s %s"
-											 org-social-file
-											 "conmanBananerd:/home/conman/prod/caddy/site/shom.dev/social.org")
-							 nil 0))))
