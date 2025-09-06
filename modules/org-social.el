f1c08d09c222581a4a4fb55f8f290310d5345005
Shom Bandopadhaya <shom@bandopadhaya.com>
2025-09-06 18:33:14 -0500

    Changed location of social.org file

--- modules/org-social.el
+++ modules/org-social.el
@@ -2,7 +2,7 @@
 	 :straight( :package "org-social"
 							:type git :host github :repo "tanrax/org-social.el")
 	:config
-	(setq org-social-file "/ssh:conmanBananerd:/home/conman/prod/caddy/site/shom.dev/social.org")
+	(setq org-social-file "/ssh:conmanBananerd:/home/conman/prod/caddy/site/org-social/social.org")
 	(add-to-list 'evil-emacs-state-modes 'org-social-timeline-mode)
 	(evil-leader/set-key
 		"s" '("social"     . (keymap))
